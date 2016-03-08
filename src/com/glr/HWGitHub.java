5bf1481863d6e8bdab169edc3977082af391a353
Gary Regier <gregier@us.ibm.com>
2016-03-07 17:50:49 -0800

    initial commit

---
+++ src/com/glr/HWGitHub.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.glr;
+
+/**
+ *  HWGitHub
+ *
+ * @author gregier
+ * @date   Mar 7, 2016
+ */
+public class HWGitHub {
+	public static void main(String[] args) {
+		System.out.println("Howdy");
+	}
+}
